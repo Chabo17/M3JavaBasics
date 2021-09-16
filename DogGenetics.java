f44a25a43765c6fd68e1bfe3ceb841dc7ac1ea9b
Chandler Bottomley <bottomleychad@gmail.com>
2021-09-16 10:16:46 -0700

    Update DogGenetics.java

--- DogGenetics.java
+++ DogGenetics.java
@@ -7,13 +7,13 @@ public class DogGenetics {
         Scanner Scan = new Scanner(System.in);
         Random Ran = new Random();
         
-        String dogo;
+        String dogName;
         String [] Breeds = {"St. bernard", "Chihuahua", "Dramatic RedNosed Asian Pug", "Common Cur","King Doberman"};
         int per =0, rando =0, curve = 20;
 
         System.out.print("What is you dog's name? ");
-        dogo = Scan.nextLine();
-        System.out.println("Well then, I have this highly reliable report on " + dogo+"'s prestigious background right here.");
+        dogName = Scan.nextLine();
+        System.out.println("Well then, I have this highly reliable report on " + dogName+"'s prestigious background right here.");
         for(int i =0;i<4;i++){
             rando = Ran.nextInt(100-per-curve);
             per+=rando;
